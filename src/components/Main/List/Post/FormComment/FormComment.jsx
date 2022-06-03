f4d77d894674cfd6393797308fdca7e60151ed41
helpMeRise <rises128@gmail.com>
2022-06-03 13:51:37 +0300

    Необязательное задание 8 урока

--- src/components/Main/List/Post/FormComment/FormComment.jsx
+++ src/components/Main/List/Post/FormComment/FormComment.jsx
@@ -1,22 +1,42 @@
 import style from './FormComment.module.css';
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 
 export const FormComment = () => {
   const inputRef = useRef(null);
+  // const focusRef = useRef(null);
+  const [isTextArea, setIsTextArea] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log(inputRef.current.value);
   };
 
+  const handleClick = e => {
+    setIsTextArea(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 0);
+  };
+
+
   return (
-    <form className={style.form} onSubmit={handleSubmit}>
-      <textarea className={style.textarea} ref={inputRef}/>
-      <button type='submit'
-        className={style.btn}
-      >
-        Отправить
-      </button>
-    </form>
+    <>
+      {!isTextArea ? (
+        <button
+          className={style.btn}
+          onClick={handleClick}>
+          Написать комментарий
+        </button>
+      ) : (
+        <form className={style.form} onSubmit={handleSubmit}>
+          <textarea className={style.textarea} ref={inputRef}/>
+          <button type='submit'
+            className={style.btn}
+          >
+            Отправить
+          </button>
+        </form>
+      )}
+    </>
   );
 };
